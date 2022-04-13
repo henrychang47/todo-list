f847181afba7d7e175aa43bbb973d034f0a81306
henrychang47 <henry881116@gmail.com>
2022-04-13 13:20:00 +0800

    add task form

--- src/modules/tasksDisplay/newTaskForm.js
+++ src/modules/tasksDisplay/newTaskForm.js
@@ -1,25 +1,40 @@
 import './newTaskForm.css';
 import data from '../data';
-import { displayList } from '.';
+import Task from '../task';
+import { displayTasks } from '.';
 const newTaskButton = document.querySelector('.tasksDisplay-newProjectButton');
 const background = document.querySelector('.maskBackground');
 const form = document.querySelector('.newTaskForm');
 const cancelButton = form.querySelector('.newTaskForm-cancelButton');
+const taskTitle = form.querySelector('#taskTitleInput');
+const taskDescription = form.querySelector('#taskDescriptionInput');
+const taskTime = form.querySelector('#taskTimeInput');
+const taskPriority = form.querySelector('#taskPriorityInput');
+const priorityHigh = form.querySelector('.priorityHigh');
+const priorityMedium = form.querySelector('.priorityMedium');
+const priorityLow = form.querySelector('.priorityLow');
+const submitButton = form.querySelector('.newTaskForm-submitButton');
+const warnningText = form.querySelector('.newTaskForm-Warnning');
 
-// const input = form.querySelector('input');
-// const submitButton = form.querySelector('.newTaskForm-submitButton');
 // const warnMessage = form.querySelector('.newTaskForm-warnMessage');
 const sideBar = document.querySelector('.sideBar');
 const tasksDisplay = document.querySelector('.tasksDisplay');
 
 newTaskButton.addEventListener('click', showForm);
 cancelButton.addEventListener('click', hideForm);
-// submitButton.addEventListener('click', submitForm);
+submitButton.addEventListener('click', submitForm);
 setTimeout(() => {
   form.style.transition = 'all 0.3s';
 }, 300);
 
+[priorityHigh, priorityMedium, priorityLow].forEach(priorityBox => {
+  priorityBox.addEventListener('click', e => {
+    selectPriority(e.target.classList[0]);
+  });
+});
+
 function showForm() {
+  if (data.currentProject === null) return;
   background.style.visibility = 'visible';
   form.style.visibility = 'visible';
   form.style.transform = 'scale(1)';
@@ -33,30 +48,88 @@ function hideForm() {
   form.style.transform = 'scale(0)';
   sideBar.style.filter = 'none';
   tasksDisplay.style.filter = 'none';
+
+  taskTitle.value = '';
+  taskDescription.value = '';
+  taskTime.value = '';
+  selectPriority('priorityMedium');
 }
 
-// function submitForm(e) {
-//   if (e.type === 'keydown' & e.key !== 'Enter') return;
+function selectPriority(priority) {
+  const setlectedWidth = "70%";
+  const noneSelectedWidth = "15%";
 
-//   let inputValue = input.value;
+  switch (priority) {
+    case 'priorityHigh':
+      priorityHigh.innerText = 'High';
+      priorityMedium.innerText = '';
+      priorityLow.innerText = '';
+      // console.log(taskPriorityInput.innerText);
+      priorityHigh.style.width = setlectedWidth;
+      priorityMedium.style.width = noneSelectedWidth;
+      priorityLow.style.width = noneSelectedWidth;
+      break;
 
-//   if (inputValue.length == 0) {
-//     warn("Project name is required");
-//     return;
-//   }
+    case 'priorityMedium':
+      priorityHigh.innerText = '';
+      priorityMedium.innerText = 'Medium';
+      priorityLow.innerText = '';
+      priorityHigh.style.width = noneSelectedWidth;
+      priorityMedium.style.width = setlectedWidth;
+      priorityLow.style.width = noneSelectedWidth;
+      break;
 
-//   data.addnewTask(inputValue);
-//   displayList();
-//   hideForm();
-// }
+    case 'priorityLow':
+      priorityHigh.innerText = '';
+      priorityMedium.innerText = '';
+      priorityLow.innerText = 'Low';
+      priorityHigh.style.width = noneSelectedWidth;
+      priorityMedium.style.width = noneSelectedWidth;
+      priorityLow.style.width = setlectedWidth;
+      break;
+  }
+}
+
+function submitForm(e) {
+  let title = checkTitleInput(taskTitle.value);
+  let description = taskDescription.value;
+  let time = taskTime.value;
+  let priority = taskPriority.innerText;
+
+  if (!title) {
+    warn(taskTitle);
+    return;
+  }
+
+  if (!description) {
+    warn(taskDescription);
+    return;
+  }
+
+  data.currentProject.taskList.push(new Task(title, description, time, priority));
+  displayTasks(data.currentProject);
+  console.log(title);
+  console.log(description);
+  console.log(time);
+  console.log(priority);
+
+  hideForm();
+}
+
+function checkTitleInput(input) {
+  if (input) return input;
+
+}
 
 // function onInput() {
 //   warnMessage.innerText = '';
 //   input.style.outlineColor = 'black';
 // }
 
-// function warn(message) {
-//   warnMessage.innerText = message;
-//   input.style.outlineColor = 'red';
-//   input.focus();
-// }
+function warn(target) {
+  target.focus();
+  target.classList.add('warnning');
+  setTimeout(() => {
+    target.classList.remove('warnning');
+  }, 500);
+}
